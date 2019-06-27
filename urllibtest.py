36256cf384f961a0b96c569dbb073ef949c0dccb
pengzongge <pengzongge@PengZonggedeMacBook-Pro-2.local>
2019-06-27 17:00:40 +0800

    type

--- urllibtest.py
+++ urllibtest.py
@@ -2,3 +2,6 @@ import urllib.request
 
 response = urllib.request.urlopen("https://www.python.org")
 print(response.read().decode("utf-8"))
+print(type(response))
+print(response.getheaders())
+print(response.getheader('Server'))
