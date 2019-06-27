2d8f93e52e572620bbfb8f0cf66090068faf24b6
pengzongge <pengzongge@PengZonggedeMacBook-Pro-2.local>
2019-06-27 17:52:19 +0800

    reqt

--- urllibreqtest.py
+++ urllibreqtest.py
@@ -1,5 +1,5 @@
 import urllib.request
 
-request = urllib.request.Request('https://python.org')
+request = urllib.request.Request('https://python.org', data=None, headers={}, origin_req_host=None)
 response = urllib.request.urlopen(request)
 print(response.read().decode('utf-8'))
