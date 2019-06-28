8f5da9f7e17e24117f0408651aefae17583c2e36
pengzongge <pengzongge@PengZonggedeMacBook-Pro-2.local>
2019-06-28 10:41:00 +0800

    req

--- urllibreqtest.py
+++ urllibreqtest.py
@@ -1,5 +1,10 @@
 import urllib.request
+import urllib.parse
 
-request = urllib.request.Request('https://python.org', data=None, headers={}, origin_req_host=None)
+param = {'token':'2b82da1e1454429fa780c8bab5912627'}
+data = bytes(urllib.parse.urlencode(param), encoding='utf-8')
+request = urllib.request.Request('https://yxtest.sqkx.net/mobile/work/commonQueryControl/queryWaterRecordList.action', data=data, headers={}, origin_req_host=None)
 response = urllib.request.urlopen(request)
 print(response.read().decode('utf-8'))
+
+
