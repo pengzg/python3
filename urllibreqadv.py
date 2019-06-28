1efff343422605ca931eeafb097ef1d7628ec201
pengzongge <pengzongge@PengZonggedeMacBook-Pro-2.local>
2019-06-28 11:58:53 +0800

    cookie

--- urllibreqadv.py
+++ urllibreqadv.py
@@ -1,15 +1,21 @@
-from urllib.request import HTTPPasswordMgrWithDefaultRealm, HTTPBasicAuthHandler, build_opener
+from urllib.request import ProxyHandler, HTTPPasswordMgrWithDefaultRealm, HTTPBasicAuthHandler, build_opener
 from urllib.error import URLError
 
 username = 'username'
 password = 'password'
 url = 'http://pma-china.com'
 
+proxy_handler = ProxyHandler({
+    'http': 'http://127.0.0.1:9743',
+    'https': 'https://127.0.0.1:9743'
+})
+
 
 p = HTTPPasswordMgrWithDefaultRealm()
 p.add_password(None, url, username, password)
 auth_handler = HTTPBasicAuthHandler(p)
 opener = build_opener(auth_handler)
+opener2 = build_opener(proxy_handler)
 
 try:
     result = opener.open(url)
