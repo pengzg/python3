f595cba6e5b965f497032d7e802c1e992248ebbd
pengzongge <pengzongge@PengZonggedeMacBook-Pro-2.local>
2019-06-28 14:14:13 +0800

    cookie

--- urllibcookie.py
+++ urllibcookie.py
@@ -18,3 +18,12 @@ else :
     response = opener.open('https://www.baidu.com')
     for item in cookie:
         print(item.name+'='+item.value)
+
+
+
+cookie = http.cookiejar.LWPCookieJar()
+cookie.load('cookie.txt', ignore_discard=True, ignore_expires=True)
+handler = urllib.request.HTTPCookieProcessor(cookie)
+opener = urllib.request.build_opener(handler)
+response = opener.open('https://www.baidu.com')
+print(response.read().decode('utf-8'))
