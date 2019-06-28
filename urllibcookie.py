ff7cc574139aa4733efec6931a84328722043437
pengzongge <pengzongge@PengZonggedeMacBook-Pro-2.local>
2019-06-28 14:08:55 +0800

    cookie

--- urllibcookie.py
+++ urllibcookie.py
@@ -1,13 +1,20 @@
 import http.cookiejar, urllib.request
+isfile = True
+if (isfile):
+    filename = 'cookie.txt'
+    #cookie = http.cookiejar.MozillaCookieJar(filename)
+    cookie = http.cookiejar.LWPCookieJar(filename)
+    handler = urllib.request.HTTPCookieProcessor(cookie)
+    opener = urllib.request.build_opener(handler)
+    response = opener.open('https://www.baidu.com')
+    cookie.save(ignore_discard=True, ignore_expires=True)
+else :
+    cookie = http.cookiejar.CookieJar()
 
-cookie = http.cookiejar.CookieJar()
+    handler = urllib.request.HTTPCookieProcessor(cookie)
 
-handler = urllib.request.HTTPCookieProcessor(cookie)
+    opener = urllib.request.build_opener(handler)
 
-opener = urllib.request.build_opener(handler)
-
-response = opener.open('https://www.baidu.com')
-for item in cookie:
-    print(item.name+'='+item.value)
-
-    
+    response = opener.open('https://www.baidu.com')
+    for item in cookie:
+        print(item.name+'='+item.value)
