475581710e3ee076549f1ba7a52c79e4b680804c
pengzongge <pengzongge@PengZonggedeMacBook-Pro-2.local>
2019-07-03 18:03:07 +0800

    d

--- requesttest.py
+++ requesttest.py
@@ -1,39 +1,53 @@
 import requests
-
+print("========get=======")
 r = requests.get('https://www.baidu.com/')
 print(type(r))
 print(r.status_code)
 print(type(r.text))
 print(r.text)
 print(r.cookies)
-
+print("========post=======")
 r = requests.post('https://www.baidu.com/')
-rint(type(r))
+print(type(r))
 print(r.status_code)
 print(type(r.text))
 print(r.text)
 print(r.cookies)
+print("========delete=======")
 r = requests.delete('https://www.baidu.com/')
-rint(type(r))
+print(type(r))
 print(r.status_code)
 print(type(r.text))
 print(r.text)
 print(r.cookies)
+print("========put=======")
 r = requests.put('https://www.baidu.com/')
-rint(type(r))
+print(type(r))
 print(r.status_code)
 print(type(r.text))
 print(r.text)
 print(r.cookies)
+print("========head=======")
 r = requests.head('https://www.baidu.com/')
-rint(type(r))
+print(type(r))
 print(r.status_code)
 print(type(r.text))
 print(r.text)
 print(r.cookies)
+print("========options=======")
 r = requests.options('https://www.baidu.com/')
-rint(type(r))
+print(type(r))
 print(r.status_code)
 print(type(r.text))
 print(r.text)
 print(r.cookies)
+
+
+data = {
+    'name': 'lee',
+    'age':22
+}
+r = requests.get('https://httpbin.org/get', params=data)
+print(r.text)
+
+
