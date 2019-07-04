dc728195dc1561cdd7f84dd0b9c5f99a3dcee596
pengzongge <pengzongge@PengZonggedeMacBook-Pro-2.local>
2019-07-04 14:19:35 +0800

    d

--- requesttest.py
+++ requesttest.py
@@ -1,4 +1,6 @@
 import requests
+import re
+
 print("========get=======")
 r = requests.get('https://www.baidu.com/')
 print(type(r))
@@ -56,6 +58,24 @@ print(type(r.text))
 print(r.json())
 print(type(r.json()))
 
+print('===============知乎===============')
+headers = {
+    'User-Agent': 'Mozilla/5.0 (Macintosh; Intel Mac OS X 10_11_4) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/52.0.2743.116 Safari/537.36'
+}
+r = requests.get('https://www.zhihu.com/explore', headers = headers)
+pattern = re.compile('explore-feed.*?question_link.*?>(.*?)</a>', re.S)
+titles = re.findall(pattern, r.text)
+print(titles)
 
+print('==============抓取二进制=====================')
+r = requests.get('https://github.com/favicon.ico')
+print('===r.text====')
+print(r.text)
+print('===r.content====')
+print(r.content)
+print('==============wb=====================')
+r = requests.get('https://github.com/favicon.ico')
+with open('favicon.ico', 'wb') as f :
+    f.write(r.content)
 
 
