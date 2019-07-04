a0f491f4332211965177be0c7cbfe1f7ea03a12f
pengzongge <pengzongge@PengZonggedeMacBook-Pro-2.local>
2019-07-04 16:59:10 +0800

    comment

--- requestadv.py
+++ requestadv.py
@@ -1,5 +1,5 @@
 import requests
-
+print('==============文件上传=================')
 files = {'file': open('favicon.ico', 'rb')}
 r = requests.post('http://httpbin.org/post', files=files)
 print(r.text)
