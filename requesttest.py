25bb29fc048decbefb46c4eb06aaf387e0519360
pengzongge <pengzongge@PengZonggedeMacBook-Pro-2.local>
2019-07-04 16:46:42 +0800

    req

--- requesttest.py
+++ requesttest.py
@@ -101,3 +101,15 @@ data = {
 }
 r = requests.post("http://httpbin.org/post", data=data)
 print(r.text)
+
+print('================响应====================')
+r = requests.get('http://www.baidu.com')
+print(type(r.status_code), r.status_code)
+print(type(r.headers), r.headers)
+print(type(r.cookies), r.cookies)
+print(type(r.url), r.url)
+print(type(r.history), r.history)
+
+r = requests.get('http://www.baidu.com')
+exit() if not r.status_code==requests.codes.ok else print('Request Successfully')
+
