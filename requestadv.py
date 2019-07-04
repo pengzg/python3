6d87eebba0a32cd6c207b3c99370dcf5ce3baec8
pengzongge <pengzongge@PengZonggedeMacBook-Pro-2.local>
2019-07-04 16:56:57 +0800

    d

--- requestadv.py
+++ requestadv.py
@@ -3,3 +3,9 @@ import requests
 files = {'file': open('favicon.ico', 'rb')}
 r = requests.post('http://httpbin.org/post', files=files)
 print(r.text)
+
+print('==============cookie=================')
+r = requests.get('http://www.baidu.com')
+print(r.cookies)
+for key, value in r.cookies.items():
+    print(key+'=='+value)
