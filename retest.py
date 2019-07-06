ebd1aa4bc90c2995f29f806da31f00d5fea38143
彭宗阁 <pzgmissyou@gmail.com>
2019-07-06 22:58:44 +0800

    compile

--- retest.py
+++ retest.py
@@ -106,3 +106,19 @@ print(type(results))
 for result in results:
     print(result)
     print(result[0], result[1], result[2])
+
+
+content = '54aK54yr5oiR54ix5L2g'
+r = re.sub('\d+', '', content)
+r = re.sub('\d+', 'x', content)
+print(r)
+
+
+content1 = '2016-12-15 12:00'
+content2 = '2016-12-17 12:55'
+content3 = '2016-12-22 13:21'
+pattern = re.compile('\d{2}:\d{2}')
+result1 = re.sub(pattern, '', content1)
+result2 = re.sub(pattern, '', content2)
+result3 = re.sub(pattern, '', content3)
+print(result1, result2, result3)
