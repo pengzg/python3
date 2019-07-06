972dc317dc68fb30ef032749c83f2f6d1ea4f4b4
彭宗阁 <pzgmissyou@gmail.com>
2019-07-06 19:09:44 +0800

    ?

--- retest.py
+++ retest.py
@@ -21,4 +21,10 @@ print(r)
 print(r.group())
 print(r.span())
 
-
+content = 'Hello 1234567 World_This is a Regex Demo'
+#r = re.match('^He.*(\d+).*Demo$', content)
+r = re.match('^He.*?(\d+).*Demo$', content)
+print(r)
+print(r.group())
+print(r.group(1))
+print(r.span())
