21c3db282797baa96270c431f85992d80f6af3bd
彭宗阁 <pzgmissyou@gmail.com>
2019-07-07 12:19:41 +0800

    mn

--- maoyan.py
+++ maoyan.py
@@ -3,6 +3,7 @@ from bs4 import BeautifulSoup
 import re
 
 offset = ['0', '10', '20', '30', '40', '50', '60', '70', '80', '90']
+#offset = ['0']
 
 def get_one_page(url):
     response = requests.get(url)
@@ -26,11 +27,13 @@ def remain():
         url = 'https://maoyan.com/board/4?offset='+i
         print(url)
         pageHtml = get_one_page(url)
-        r = re.findall('<dd>.*?title="(.*?)".*?board-img.*src="(.*?)".*?star">(.*?)</p>.*?score.*?integer">(.*?)</i>.*?fraction">(.*?)</i>', pageHtml, re.S)
-        #print(r)
+        r = re.findall('<dd>.*<img.*board-img.*src="(.*)".*>.*p.*name">.*a.*>(.*)</p>.*p.*star">(.*)</p>.*releasetime">(.*)</p>.*score.*integer">(.*)</i>.*fraction">(.*)</i>', pageHtml, re.S)
+        r = re.findall('<dd>.*?board-index.*?>(.*?)</i>.*?data-src="(.*?)".*?name.*?a.*?>(.*?)</.*?star.*?>(.*?)</.*?releasetime.*?>(.*?)</.*?integer.*?>(.*?)</.*?fraction.*?>(.*?)</.*?</dd>', pageHtml, re.S)
+        print(r)
         #print(type(r))
         for result in r:
             #print(result)
-            print(result[0], result[1], result[2], result[3],result[4])
+            #print(result[0], result[1], result[2], result[3],result[4], result[5])
+            print(result[0], result[1], result[2], result[3], result[4], result[5], result[6])
 
 remain()
