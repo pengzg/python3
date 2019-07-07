de5fd7ec77e1a08c52d6d455538f5bba4e385b45
彭宗阁 <pzgmissyou@gmail.com>
2019-07-07 16:35:22 +0800

    xpath

---
+++ xpathtest.py
@@ -0,0 +1,23 @@
+from lxml import etree
+text = '''
+<div>
+    <ul>
+         <li class="item-0"><a href="link1.html">first item</a></li>
+         <li class="item-1"><a href="link2.html">second item</a></li>
+         <li class="item-inactive"><a href="link3.html">third item</a></li>
+         <li class="item-1"><a href="link4.html">fourth item</a></li>
+         <li class="item-0"><a href="link5.html">fifth item</a>
+     </ul>
+ </div>
+'''
+html = etree.HTML(text)
+r = etree.tostring(html)
+print(r.decode('utf-8'))
+
+html = etree.parse('./test.html', etree.HTMLParser())
+r = etree.tostring(html)
+print(r.decode('utf-8'))
+
+html = etree.parse('./test.html', etree.HTMLParser())
+r = html.xpath('//*')
+print(r)
