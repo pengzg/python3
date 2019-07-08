cfe11c58a99d0ae51db94d8b5a3e249a826dd5ad
pengzongge <pengzongge@PengZonggedeMacBook-Pro-2.local>
2019-07-08 17:13:28 +0800

    soup

--- beautifulsouptest.py
+++ beautifulsouptest.py
@@ -1,4 +1,5 @@
 from bs4 import BeautifulSoup
+import re
 soup = BeautifulSoup('<p>Hello</p>', 'lxml')
 print(soup.p.string)
 
@@ -265,7 +266,76 @@ print(soup.find_all(attrs={'id':'list-1'}))
 print(soup.find_all(attrs={'name': 'elements'}))
 
 
+print(soup.find_all(id='list-1'))
+print(soup.find_all(class_='element'))
 
 
+html='''
+<div class="panel">
+    <div class="panel-body">
+        <a>Hello, this is a link</a>
+        <a>Hello, this is a link, too</a>
+    </div>
+</div>
+'''
+soup = BeautifulSoup(html, 'lxml')
+print(soup.find_all(text=re.compile('link')))
+
+print('=================ggggggggggggggggg===================')
+html='''
+<div class="panel">
+    <div class="panel-heading">
+        <h4>Hello</h4>
+    </div>
+    <div class="panel-body">
+        <ul class="list" id="list-1">
+            <li class="element">Foo</li>
+            <li class="element">Bar</li>
+            <li class="element">Jay</li>
+        </ul>
+        <ul class="list list-small" id="list-2">
+            <li class="element">Foo</li>
+            <li class="element">Bar</li>
+        </ul>
+    </div>
+</div>
+'''
+soup = BeautifulSoup(html, 'lxml')
+print(soup.find(name='ul'))
+print(type(soup.find(name='ul')))
+print(soup.find(class_='list'))
+
+print('==============hhhhhhhhhhhhhh===================')
+
+html='''
+<div class="panel">
+    <div class="panel-heading">
+        <h4>Hello</h4>
+    </div>
+    <div class="panel-body">
+        <ul class="list" id="list-1">
+            <li class="element">Foo</li>
+            <li class="element">Bar</li>
+            <li class="element">Jay</li>
+        </ul>
+        <ul class="list list-small" id="list-2">
+            <li class="element">Foo</li>
+            <li class="element">Bar</li>
+        </ul>
+    </div>
+</div>
+'''
+soup = BeautifulSoup(html, 'lxml')
+print(soup.select('.panel .panel-heading'))
+print(soup.select('ul li'))
+print(soup.select('#list-2 .element'))
+print(type(soup.select('ul')[0]))
+
+
+for ul in soup.select('ul'):
+    print(ul.select('li'))
+
+
+    
 
 
