6183974773bc43568196301762b7bc88cc6d5940
pengzongge <pengzongge@PengZonggedeMacBook-Pro-2.local>
2019-07-08 15:50:32 +0800

    soup

--- beautifulsouptest.py
+++ beautifulsouptest.py
@@ -1,3 +1,62 @@
 from bs4 import BeautifulSoup
 soup = BeautifulSoup('<p>Hello</p>', 'lxml')
 print(soup.p.string)
+
+
+html = """
+<html><head><title>The Dormouse's story</title></head>
+<body>
+<p class="title" name="dromouse"><b>The Dormouse's story</b></p>
+<p class="story">Once upon a time there were three little sisters; and their names were
+<a href="http://example.com/elsie" class="sister" id="link1"><!-- Elsie --></a>,
+<a href="http://example.com/lacie" class="sister" id="link2">Lacie</a> and
+<a href="http://example.com/tillie" class="sister" id="link3">Tillie</a>;
+and they lived at the bottom of a well.</p>
+<p class="story">...</p>
+"""
+
+soup = BeautifulSoup(html, 'lxml')
+print(soup.prettify())
+print(soup.title.string)
+
+print("============3333333333================")
+html = """
+<html><head><title>The Dormouse's story</title></head>
+<body>
+<p class="title" name="dromouse"><b>The Dormouse's story</b></p>
+<p class="story">Once upon a time there were three little sisters; and their names were
+<a href="http://example.com/elsie" class="sister" id="link1"><!-- Elsie --></a>,
+<a href="http://example.com/lacie" class="sister" id="link2">Lacie</a> and
+<a href="http://example.com/tillie" class="sister" id="link3">Tillie</a>;
+and they lived at the bottom of a well.</p>
+<p class="story">...</p>
+"""
+
+soup = BeautifulSoup(html, 'lxml')
+print(soup.title)
+print(type(soup.title))
+print(soup.title.string)
+print(soup.head)
+print(soup.p)
+print(soup.title.name)
+
+print('================4444444444=================')
+print(soup.p.attrs)
+print(soup.p.attrs['name'])
+
+print('================55555555555=================')
+print(soup.p['name'])
+print(soup.p['class'])
+
+print('================66666666666======================')
+print(soup.p.string)
+
+print('================66666666666========================')
+html = """
+<html><head><title>The Dormouse's story</title></head>
+<body>
+"""
+soup = BeautifulSoup(html, 'lxml')
+print(soup.head.title)
+print(type(soup.head.title))
+print(soup.head.title.string)
