46a7bef633b40a7c310ad7e6751d583f7078b46d
pengzongge <pengzongge@PengZonggedeMacBook-Pro-2.local>
2019-07-08 16:18:00 +0800

    siblings

--- beautifulsouptest.py
+++ beautifulsouptest.py
@@ -152,5 +152,40 @@ html = """
 soup = BeautifulSoup(html, 'lxml')
 print(soup.a.parent)
 
+print('=================bbbbbbbbbbbbbbbbbbb===================')
+html = """
+<html>
+    <body>
+        <p class="story">
+            <a href="http://example.com/elsie" class="sister" id="link1">
+                <span>Elsie</span>
+            </a>
+        </p>
+"""
+soup = BeautifulSoup(html, 'lxml')
+print(type(soup.a.parents))
+print(list(enumerate(soup.a.parents)))
 
+print('====================cccccccccccccccccccc=====================')
 
+html = """
+<html>
+    <body>
+        <p class="story">
+            Once upon a time there were three little sisters; and their names were
+            <a href="http://example.com/elsie" class="sister" id="link1">
+                <span>Elsie</span>
+            </a>
+            Hello
+            <a href="http://example.com/lacie" class="sister" id="link2">Lacie</a> 
+            and
+            <a href="http://example.com/tillie" class="sister" id="link3">Tillie</a>
+            and they lived at the bottom of a well.
+        </p>
+"""
+
+soup = BeautifulSoup(html, 'lxml')
+print('Next Sibling', soup.a.next_sibling)
+print('Prev Sibling', soup.a.previous_sibling)
+print('Next Siblings', list(enumerate(soup.a.next_siblings)))
+print('Prev Siblings', list(enumerate(soup.a.previous_siblings)))
