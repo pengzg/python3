8661826128a1682fb1fe14db051c9867c2cc44da
pengzongge <pengzongge@PengZonggedeMacBook-Pro-2.local>
2019-07-08 16:06:16 +0800

    soup

--- beautifulsouptest.py
+++ beautifulsouptest.py
@@ -84,9 +84,73 @@ soup = BeautifulSoup(html, 'lxml')
 print(soup.p.contents)
 
 print('================888888888888888888==============')
+html = """
+<html>
+    <head>
+        <title>The Dormouse's story</title>
+    </head>
+    <body>
+        <p class="story">
+            Once upon a time there were three little sisters; and their names were
+            <a href="http://example.com/elsie" class="sister" id="link1">
+                <span>Elsie</span>
+            </a>
+            <a href="http://example.com/lacie" class="sister" id="link2">Lacie</a> 
+            and
+            <a href="http://example.com/tillie" class="sister" id="link3">Tillie</a>
+            and they lived at the bottom of a well.
+        </p>
+        <p class="story">...</p>
+"""
+soup = BeautifulSoup(html, 'lxml')
+print(soup.p.children)
+for i, child in enumerate(soup.p.children):
+    print(i, child)
 
 
+print('====================999999999999999999=======================')
+html = """
+<html>
+    <head>
+        <title>The Dormouse's story</title>
+    </head>
+    <body>
+        <p class="story">
+            Once upon a time there were three little sisters; and their names were
+            <a href="http://example.com/elsie" class="sister" id="link1">
+                <span>Elsie</span>
+            </a>
+            <a href="http://example.com/lacie" class="sister" id="link2">Lacie</a> 
+            and
+            <a href="http://example.com/tillie" class="sister" id="link3">Tillie</a>
+            and they lived at the bottom of a well.
+        </p>
+        <p class="story">...</p>
+"""
+soup = BeautifulSoup(html, 'lxml')
+print(soup.p.descendants)
+for i, child in enumerate(soup.p.descendants):
+    print(i, child)
 
+
+print('=================aaaaaaaaaaaaaaaaaaa======================')
+
+html = """
+<html>
+    <head>
+        <title>The Dormouse's story</title>
+    </head>
+    <body>
+        <p class="story">
+            Once upon a time there were three little sisters; and their names were
+            <a href="http://example.com/elsie" class="sister" id="link1">
+                <span>Elsie</span>
+            </a>
+        </p>
+        <p class="story">...</p>
+"""
+soup = BeautifulSoup(html, 'lxml')
+print(soup.a.parent)
 
 
 
