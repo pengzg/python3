dd790269bd5cdc42fdf05da0615b55e41c05fa87
pengzongge <pengzongge@PengZonggedeMacBook-Pro-2.local>
2019-07-08 16:25:50 +0800

    class

--- beautifulsouptest.py
+++ beautifulsouptest.py
@@ -189,3 +189,24 @@ print('Next Sibling', soup.a.next_sibling)
 print('Prev Sibling', soup.a.previous_sibling)
 print('Next Siblings', list(enumerate(soup.a.next_siblings)))
 print('Prev Siblings', list(enumerate(soup.a.previous_siblings)))
+
+print('==================dddddddddddddddddddd=======================')
+html = """
+<html>
+    <body>
+        <p class="story">
+            Once upon a time there were three little sisters; and their names were
+            <a href="http://example.com/elsie" class="sister" id="link1">Bob</a><a href="http://example.com/lacie" class="sister" id="link2">Lacie</a> 
+        </p>
+"""
+soup = BeautifulSoup(html, 'lxml')
+print('Next Sibling:')
+print(type(soup.a.next_sibling))
+print(soup.a.next_sibling)
+print(soup.a.next_sibling.string)
+print('Parent:')
+print(type(soup.a.parents))
+print(list(soup.a.parents)[0])
+print(list(soup.a.parents)[0].attrs['class'])
+
+
