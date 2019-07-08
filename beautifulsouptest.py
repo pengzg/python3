00c13f3f43c9e96ffaa5fbb97ab3c95f46ff3e21
pengzongge <pengzongge@PengZonggedeMacBook-Pro-2.local>
2019-07-08 15:54:57 +0800

    d

--- beautifulsouptest.py
+++ beautifulsouptest.py
@@ -60,3 +60,33 @@ soup = BeautifulSoup(html, 'lxml')
 print(soup.head.title)
 print(type(soup.head.title))
 print(soup.head.title.string)
+
+print('================777777777777777===============')
+html = """
+<html>
+    <head>
+        <title>The Dormouse's story</title>
+    </head>
+    <body>
+        <p class="story">
+            Once upon a time there were three little sisters; and their names were
+            <a href="http://example.com/elsie" class="sister" id="link1">
+                <span>Elsie</span>
+            </a>
+            <a href="http://example.com/lacie" class="sister" id="link2">Lacie</a> 
+            and
+            <a href="http://example.com/tillie" class="sister" id="link3">Tillie</a>
+            and they lived at the bottom of a well.
+        </p>
+        <p class="story">...</p>
+"""
+soup = BeautifulSoup(html, 'lxml')
+print(soup.p.contents)
+
+print('================888888888888888888==============')
+
+
+
+
+
+
