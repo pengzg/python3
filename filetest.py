272bc536c2f40b471907b214b349abe186384e4d
pengzongge <pengzongge@PengZonggedeMacBook-Pro-2.local>
2019-07-09 10:47:18 +0800

    file

--- filetest.py
+++ filetest.py
@@ -7,3 +7,13 @@ headers = {
 }
 
 html = requests.get(url, headers=headers).text
+doc = pq(html)
+items = doc('.explore-tab .feed-item').items()
+for item in items:
+    question = item.find('h2').text()
+    author = item.find('.author-link-line').text()
+    answer = pq(item.find('.content').html()).text()
+    file = open('explore.txt', 'a', encoding='utf-8')
+    file.write('\n'.join([question, author, answer]))
+    file.write('\n'+ '=' * 50 + '\n')
+    file.close()
