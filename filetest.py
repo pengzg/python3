9e9d1dfd7f12f42ed0707c36439c76fa3824b83f
pengzongge <pengzongge@PengZonggedeMacBook-Pro-2.local>
2019-07-09 10:34:52 +0800

    file

---
+++ filetest.py
@@ -0,0 +1,9 @@
+import requests
+from pyquery import PyQuery as pq 
+
+url = 'https://www.zhihu.com/explore'
+headers = {
+    'User-Agent': 'Mozilla/5.0 (Macintosh; Intel Mac OS X 10_12_3) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/58.0.3029.110 Safari/537.36'
+}
+
+html = requests.get(url, headers=headers).text
