3427b09a5aacd728d6c1e3c991490d9417ca9ae6
pengzongge <pengzongge@PengZonggedeMacBook-Pro-2.local>
2019-07-09 13:37:38 +0800

    d

--- jsontest.py
+++ jsontest.py
@@ -30,10 +30,10 @@ str = '''
 '''
 #print(json.loads(str))
 
-with open('data.json', 'r') as file:
-    str = file.read()
-    data = json.loads(str)
-    print(data)
+# with open('data.json', 'r') as file:
+#     str = file.read()
+#     data = json.loads(str)
+#     print(data)
 
 data = [{
     'name': 'Bob',
@@ -41,11 +41,18 @@ data = [{
     'birthday': '1992-10-18'
 }]
 
-with open('data.json', 'w') as file:
-    file.write(json.dumps(data))
-    file.write(json.dumps(data, indent=2))
+#with open('data.json', 'w') as file:
+    #file.write(json.dumps(data))
+    #file.write(json.dumps(data, indent=2))
 
 
+data = [{
+    'name': '张明',
+    'gender': '男',
+    'birthday': '1992-10-18'
+}]
 
-
+with open('data.json', 'w', encoding='utf-8') as file:
+  #  file.write(json.dumps(data))
+    file.write(json.dumps(data, indent=2, ensure_ascii=False))
 
