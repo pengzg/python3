571b8fb8713de09176a1ca3a5405617d9f338452
pengzongge <pengzongge@PengZonggedeMacBook-Pro-2.local>
2019-07-09 13:55:16 +0800

    csv

--- csvtest.py
+++ csvtest.py
@@ -2,6 +2,9 @@ import csv
 
 
 with open('data.csv', 'w') as csvfile:
-    writer = csv.writer(csvfile)
+    
+    writer = csv.writer(csvfile, delimiter=',')
+    # writer = csv.writer(csvfile)
     writer.writerow(['id', 'name', 'age'])
     writer.writerow(['001', 'mike', 30])
+    writer.writerows([['002', 'lee', '30'], ['003', 'zhang', '40']])
