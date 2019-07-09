fc2391fbe714dbc4af98675f56bf63b9feb43091
pengzongge <pengzongge@PengZonggedeMacBook-Pro-2.local>
2019-07-09 14:43:35 +0800

    mysql

--- mysqltest.py
+++ mysqltest.py
@@ -1,8 +1,31 @@
 import pymysql
-db = pymysql.connect(host='', user='root', password='', port=3306)
+host = ''
+pwd = ''
+
+# db = pymysql.connect(host=host, user='root', password=pwd, port=3306)
+# cursor = db.cursor()
+# cursor.execute('SELECT VERSION()')
+# data = cursor.fetchone()
+# print('Database Version', data)
+# cursor.execute('CREATE DATABASE spiders DEFAULT CHARACTER SET utf8')
+# db.close()
+
+# db = pymysql.connect(host=host, user='root', password=pwd, port=3306, db='spiders')
+# cursor = db.cursor()
+# sql = 'CREATE TABLE IF NOT EXISTS students (id VARCHAR(255) NOT NULL, name VARCHAR(255) NOT NULL, age INT NOT NULL, PRIMARY KEY (id))'
+# cursor.execute(sql)
+# db.close()
+
+id = '20120001'
+user = 'Bob'
+age = 20
+ 
+db = pymysql.connect(host=host, user='root', password=pwd, port=3306, db='spiders')
 cursor = db.cursor()
-cursor.execute('SELECT VERSION()')
-data = cursor.fetchone()
-print('Database Version', data)
-cursor.execute('CREATE DATABASE spiders DEFAULT CHARACTER SET utf8')
+sql = 'INSERT INTO students(id, name, age) values(%s, %s, %s)'
+try:
+    cursor.execute(sql, (id, user, age))
+    db.commit()
+except:
+    db.rollback()
 db.close()
