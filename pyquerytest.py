89ae0ddf7c259372134e969b5ef884e68c499571
pengzongge <pengzongge@PengZonggedeMacBook-Pro-2.local>
2019-07-09 09:06:49 +0800

    com

--- pyquerytest.py
+++ pyquerytest.py
@@ -119,7 +119,53 @@ for li in lis:
 
 print('=================66666666666666666666===============')
 
+html = '''
+<div class="wrap">
+    <div id="container">
+        <ul class="list">
+             <li class="item-0">first item</li>
+             <li class="item-1"><a href="link2.html">second item</a></li>
+             <li class="item-0 active"><a href="link3.html"><span class="bold">third item</span></a></li>
+             <li class="item-1 active"><a href="link4.html">fourth item</a></li>
+             <li class="item-0"><a href="link5.html">fifth item</a></li>
+         </ul>
+     </div>
+ </div>
+'''
+doc = pq(html)
+a = doc('.item-0.active a')
+print(a, type(a))
+print(a.attr('href'))
+print(a.attr.href)
 
+
+a = doc('a')
+for item in a.items():
+    print(item.attr('href'))
+
+html = '''
+<div class="wrap">
+    <div id="container">
+        <ul class="list">
+             <li class="item-0">first item</li>
+             <li class="item-1"><a href="link2.html">second item</a></li>
+             <li class="item-0 active"><a href="link3.html"><span class="bold">third item</span></a></li>
+             <li class="item-1 active"><a href="link4.html">fourth item</a></li>
+             <li class="item-0"><a href="link5.html">fifth item</a></li>
+         </ul>
+     </div>
+ </div>
+'''
+
+
+doc = pq(html)
+a = doc('.item-0.acitve a')
+print(a)
+print(a.text())
+
+span = doc('.item-0.active a span')
+print(span)
+print(span.text())
 
 
 
