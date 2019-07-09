1ad79bc705531f2d05a45e2e7995b3d75e05dda7
pengzongge <pengzongge@PengZonggedeMacBook-Pro-2.local>
2019-07-09 09:14:21 +0800

    777777

--- pyquerytest.py
+++ pyquerytest.py
@@ -167,5 +167,51 @@ span = doc('.item-0.active a span')
 print(span)
 print(span.text())
 
+doc = pq(html)
+li = doc('.item-0.active')
+print(li)
+print(li.html())
+
+html = '''
+<div class="wrap">
+    <div id="container">
+        <ul class="list">
+             <li class="item-1"><a href="link2.html">second item</a></li>
+             <li class="item-0 active"><a href="link3.html"><span class="bold">third item</span></a></li>
+             <li class="item-1 active"><a href="link4.html">fourth item</a></li>
+             <li class="item-0"><a href="link5.html">fifth item</a></li>
+         </ul>
+     </div>
+ </div>
+'''
+doc = pq(html)
+li = doc('li')
+print(li.html())
+print(li.text())
+print(type(li.text()))
+
+print('==================777777777777777777===================')
+
+html = '''
+<div class="wrap">
+    <div id="container">
+        <ul class="list">
+             <li class="item-0">first item</li>
+             <li class="item-1"><a href="link2.html">second item</a></li>
+             <li class="item-0 active"><a href="link3.html"><span class="bold">third item</span></a></li>
+             <li class="item-1 active"><a href="link4.html">fourth item</a></li>
+             <li class="item-0"><a href="link5.html">fifth item</a></li>
+         </ul>
+     </div>
+ </div>
+'''
+doc = pq(html)
+li = doc('.item-0.active')
+print(li)
+li.removeClass('active')
+print(li)
+li.addClass('active')
+print(li)
+
 
 
