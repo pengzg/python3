4655d5756ac1e4b0b159986f2755cbf55507cdf7
pengzongge <pengzongge@PengZonggedeMacBook-Pro-2.local>
2019-07-09 11:06:38 +0800

    file

--- filetest.py
+++ filetest.py
@@ -9,11 +9,30 @@ headers = {
 html = requests.get(url, headers=headers).text
 doc = pq(html)
 items = doc('.explore-tab .feed-item').items()
-for item in items:
-    question = item.find('h2').text()
-    author = item.find('.author-link-line').text()
-    answer = pq(item.find('.content').html()).text()
-    file = open('explore.txt', 'a', encoding='utf-8')
-    file.write('\n'.join([question, author, answer]))
-    file.write('\n'+ '=' * 50 + '\n')
-    file.close()
+def filew2(items):
+    with open('explore.txt', 'a', encoding='utf-8') as file:
+        for item in items:
+            question = item.find('h2').text()
+            author = item.find('.author-link-line').text()
+            answer = pq(item.find('.content').html()).text()
+            # 文件写入1
+            #file = open('explore.txt', 'a', encoding='utf-8')
+            #file.write('\n'.join([question, author, answer]))
+            #file.write('\n'+ '=' * 50 + '\n')
+            # 文件写入2
+        
+            file.write('\n'.join([question, author, answer]))
+            file.write('\n'+ '='*50+'\n')
+
+def filew(items):  
+    for item in items:
+            question = item.find('h2').text()
+            author = item.find('.author-link-line').text()
+            answer = pq(item.find('.content').html()).text()
+            # 文件写入1
+            file = open('explore.txt', 'a', encoding='utf-8')
+            file.write('\n'.join([question, author, answer]))
+            file.write('\n'+ '=' * 50 + '\n')
+            file.close()
+
+filew(items)
