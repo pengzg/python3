639709de9d667373cd7f658c589c4885004455e0
pengzongge <pengzongge@PengZonggedeMacBook-Pro-2.local>
2019-07-09 14:59:23 +0800

    insert

--- mysqltest.py
+++ mysqltest.py
@@ -16,16 +16,39 @@ pwd = ''
 # cursor.execute(sql)
 # db.close()
 
-id = '20120001'
-user = 'Bob'
-age = 20
+
+
+# id = '20120001'
+# user = 'Bob'
+# age = 20
  
-db = pymysql.connect(host=host, user='root', password=pwd, port=3306, db='spiders')
-cursor = db.cursor()
-sql = 'INSERT INTO students(id, name, age) values(%s, %s, %s)'
-try:
-    cursor.execute(sql, (id, user, age))
-    db.commit()
-except:
-    db.rollback()
-db.close()
+# db = pymysql.connect(host=host, user='root', password=pwd, port=3306, db='spiders')
+# cursor = db.cursor()
+# sql = 'INSERT INTO students(id, name, age) values(%s, %s, %s)'
+# try:
+#     cursor.execute(sql, (id, user, age))
+#     db.commit()
+# except:
+#     db.rollback()
+# db.close()
+
+# db = pymysql.connect(host=host, user='root', password=pwd, port=3306, db='spiders')
+# cursor = db.cursor()
+# data = {
+#     'id': '20120005',
+#     'name': 'Bob',
+#     'age': 20
+# }
+# table = 'students'
+# keys = ', '.join(data.keys())
+# values = ', '.join(['%s'] * len(data))
+# sql = 'INSERT INTO {table}({keys}) VALUES ({values})'.format(table=table, keys=keys, values=values)
+# print(sql)
+# try:
+#    if cursor.execute(sql, tuple(data.values())):
+#        print('Successful')
+#        db.commit()
+# except:
+#     print('Failed')
+#     db.rollback()
+# db.close()
