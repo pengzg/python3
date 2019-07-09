bcce9b80c7739a21625038ec278b8927c5e7655c
pengzongge <pengzongge@PengZonggedeMacBook-Pro-2.local>
2019-07-09 11:36:49 +0800

    d

--- jsontest.py
+++ jsontest.py
@@ -15,3 +15,37 @@ print(type(str))
 data = json.loads(str)
 print(data)
 print(type(data))
+print(data[0]['name'])
+print(data[0].get('name'))
+
+print(data[0].get('age'))
+print(data[0].get('age', 25))
+
+str = '''
+[{
+    'name': 'Bob',
+    'gender': 'male',
+    'birthday': '1992-10-18'
+}]
+'''
+#print(json.loads(str))
+
+with open('data.json', 'r') as file:
+    str = file.read()
+    data = json.loads(str)
+    print(data)
+
+data = [{
+    'name': 'Bob',
+    'gender': 'male',
+    'birthday': '1992-10-18'
+}]
+
+with open('data.json', 'w') as file:
+    file.write(json.dumps(data))
+    file.write(json.dumps(data, indent=2))
+
+
+
+
+
