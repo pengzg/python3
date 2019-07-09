93f469febefa3b9d77f9016983cb83bb65224c28
pengzongge <pengzongge@PengZonggedeMacBook-Pro-2.local>
2019-07-09 16:55:13 +0800

    mongo

--- mongotest.py
+++ mongotest.py
@@ -1,6 +1,67 @@
 import pymongo
+from bson.objectid import ObjectId
 
 #export PATH=/usr/local/mongodb/bin:$PATH
 host = ''
 client = pymongo.MongoClient(host=host, port=27017)
 print(client)
+db = client.test
+collection = db.students
+student = {
+    'id': '20170107',
+    'name': 'Jordan',
+    'age': 20,
+    'gender': 'male'
+}
+r = collection.insert(student)
+print(r)
+student1 = {
+    'id': '20170104',
+    'name': 'Jordan',
+    'age': 20,
+    'gender': 'male'
+}
+ 
+student2 = {
+    'id': '20170206',
+    'name': 'Mike',
+    'age': 21,
+    'gender': 'male'
+}
+r = collection.insert([student1, student2])
+print(r)
+student = {
+    'id': '20170107',
+    'name': 'Jordan',
+    'age': 20,
+    'gender': 'male'
+}
+r = collection.insert_one(student)
+print(r)
+student1 = {
+    'id': '20170104',
+    'name': 'Jordan',
+    'age': 20,
+    'gender': 'male'
+}
+ 
+student2 = {
+    'id': '20170206',
+    'name': 'Mike',
+    'age': 21,
+    'gender': 'male'
+}
+r = collection.insert_many([student1, student2])
+print(r)
+print(r.inserted_ids)
+
+
+r = collection.find_one({'name':'Mike'})
+print(type(r))
+print(r)
+
+
+
+
+
+
