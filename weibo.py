7cc43eaa11142fdb4b7f4b6a84af65520712dbd5
pengzongge <pengzongge@PengZonggedeMacBook-Pro-2.local>
2019-07-10 11:21:26 +0800

    weibo

--- weibo.py
+++ weibo.py
@@ -1,2 +1,27 @@
 from urllib.parse import urlencode
 import requests
+
+base_url = 'https://m.weibo.cn/api/container/getIndex?'
+
+headers = {
+    'Host': 'm.weibo.cn',
+    'Referer': 'https://m.weibo.cn/u/2830678474',
+    'User-Agent': 'Mozilla/5.0 (Macintosh; Intel Mac OS X 10_12_3) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/58.0.3029.110 Safari/537.36',
+    'X-Requested-With': 'XMLHttpRequest',
+}
+
+def get_one_page(page):
+    params = {
+        'type': 'uid',
+        'value': '2830678474',
+        'containerid': '1076032830678474',
+        'page': page
+    }
+    print(params)
+    url = base_url + urlencode(params)
+    try:
+        response = requests.get(url, headers = headers)
+        if response.status_code ==200:
+            return response.json()
+    except requests.ConnectionError as e:
+        print('Error', e.args)
