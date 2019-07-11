85f5a6cb12562448c587344ffca1424bfed19e46
彭宗阁 <pzgmissyou@gmail.com>
2019-07-11 19:50:39 +0800

    d

--- qb.py
+++ qb.py
@@ -2,26 +2,36 @@
 import requests;
 from bs4 import BeautifulSoup
 
-
+baseUrl = 'https://www.231ta.com';
 
 filename = "data.txt"
 
 with open(filename,'a') as f:
 	for i in range(1,30): 
 		print(i)
-		url = "https://www.697www.com/Html/60s/index-"+str(i)+".html"
+		url = baseUrl+"/Html/60/index-"+str(i)+".html"
 		#url = "https://www.qiushibaike.com/imgrank/page/"+str(i)
 
 		wbdata = requests.get(url)
-		print(wbdata.apparent_encoding)
+		# print(wbdata.apparent_encoding)
 		wbdata.encoding = 'UTF-8-SIG'
 		webData = wbdata.text
 		# 对获取到的文本进行解析
 		soup = BeautifulSoup(webData,'lxml')
 		# print(soup)
 		# 从解析文件中通过select选择器定位指定的元素，返回一个列表
-		news_titles = soup.select("div.movie_list ul li a h3")
+		# news_titles = soup.select("div.movie_list ul li a h3")
 
-		for n in news_titles:
-			print(n.get_text())
-			f.write(n.get_text()+"\n")
+		# for n in news_titles:
+		# 	print(n.get_text())
+		# 	f.write(n.get_text()+"\n")
+
+		items = soup.select("div.movie_list ul li")
+
+		for n in items:
+			print(n.find('h3').get_text())
+			print(baseUrl+n.a.attrs['href'])
+
+			# f.write(n.a.attrs['href'])
+
+			# f.write(n.select('a h3')[0].get_text()+"\n")
