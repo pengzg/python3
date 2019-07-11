85b34c388be5fe2e3efb7c87393325436fcd91e0
pengzongge <pengzongge@PengZonggedeMacBook-Pro-2.local>
2019-07-11 14:05:27 +0800

    drag

--- seleniumtest.py
+++ seleniumtest.py
@@ -43,12 +43,17 @@ import time
 
 
 #进行控制浏览器操作
+# browser = webdriver.Chrome()
+# browser.get('https://www.taobao.com')
+# input = browser.    ('q')
+# input.send_keys('iPhone')
+# time.sleep(1)
+# input.clear()
+# input.send_keys('iPad')
+# button = browser.find_element_by_class_name('btn-search')
+# button.click()
+
 browser = webdriver.Chrome()
-browser.get('https://www.taobao.com')
-input = browser.find_element_by_id('q')
-input.send_keys('iPhone')
-time.sleep(1)
-input.clear()
-input.send_keys('iPad')
-button = browser.find_element_by_class_name('btn-search')
-button.click()
+browser.get('https://www.zhihu.com/explore')
+browser.execute_script('window.scrollTo(0, document.body.scrollHeight)')
+browser.execute_script('alert("To Bottom")')
