2861eb0dd281c8f98f7d1a8e8dba57649db18b50
pengzongge <pengzongge@PengZonggedeMacBook-Pro-2.local>
2019-07-11 11:32:06 +0800

    sele

--- seleniumtest.py
+++ seleniumtest.py
@@ -3,6 +3,7 @@ from selenium.webdriver.common.by import By
 from selenium.webdriver.common.keys import Keys
 from selenium.webdriver.support import expected_conditions as EC
 from selenium.webdriver.support.wait import WebDriverWait
+import time
 #  浏览器支持
 # browser = webdriver.Chrome()
 # browser = webdriver.Firefox()
@@ -10,21 +11,44 @@ from selenium.webdriver.support.wait import WebDriverWait
 # browser = webdriver.PhantomJS()
 # browser = webdriver.Safari()
 
-browser = webdriver.Chrome()
-try:
-    browser.get('https://www.baidu.com')
-    input = browser.find_element_by_id('kw')
-    input.send_keys('Python')
-    input.send_keys(Keys.ENTER)
-    wait = WebDriverWait(browser, 10)
-    wait.until(EC.presence_of_element_located((By.ID, 'content_left')))
-    print(browser.current_url)
-    print(browser.get_cookies())
-    print(browser.page_source)
-finally:
-    browser.close()
+# browser = webdriver.Chrome()
+# try:
+#     browser.get('https://www.baidu.com')
+#     input = browser.find_element_by_id('kw')
+#     input.send_keys('Python')
+#     input.send_keys(Keys.ENTER)
+#     wait = WebDriverWait(browser, 10)
+#     wait.until(EC.presence_of_element_located((By.ID, 'content_left')))
+#     print(browser.current_url)
+#     print(browser.get_cookies())
+#     print(browser.page_source)
+# finally:
+#     browser.close()
 
 # browser = webdriver.Chrome()
 # browser.get('https://www.taobao.com')
 # print(browser.page_source)
 # browser.close()
+
+# 查找dom方法 browser对象
+# find_elements_by_id
+# find_elements_by_name
+# find_elements_by_xpath
+# find_elements_by_link_text
+# find_elements_by_partial_link_text
+# find_elements_by_tag_name
+# find_elements_by_class_name
+# find_elements_by_css_selector
+# find_elements(By.CSS_SELECTOR, '.service-bd li')
+
+
+#进行控制浏览器操作
+browser = webdriver.Chrome()
+browser.get('https://www.taobao.com')
+input = browser.find_element_by_id('q')
+input.send_keys('iPhone')
+time.sleep(1)
+input.clear()
+input.send_keys('iPad')
+button = browser.find_element_by_class_name('btn-search')
+button.click()
