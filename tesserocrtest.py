2c89544b87e83b08ccc7e8eb8458f7655c602488
彭宗阁 <pzgmissyou@gmail.com>
2019-07-12 22:12:33 +0800

    d

--- tesserocrtest.py
+++ tesserocrtest.py
@@ -9,5 +9,18 @@ print(tesserocr.file_to_text('./images/code.jpg'))
 
 print(tesserocr.file_to_text('./images/image.png'))
 
+img = image.convert('L')
+# img.show()
+threshold = 110
+table = []
+for i in range(256):
+    if i< threshold:
+        table.append(0)
+    else :
+        table.append(1)
+image = img.point(table, '1')
+image.show()
+res = tesserocr.image_to_text(image)
+print(res)
 
 
