38435f220363c11ee3f16c90fbf346895920fd6b
彭宗阁 <pzgmissyou@gmail.com>
2019-07-14 17:19:42 +0800

    d

--- gittest.py
+++ gittest.py
@@ -2,13 +2,9 @@ import requests
 from lxml import etree
 
 
-def token(self):
-    response = self.session.get(self.login_url, headers= self.headers)
-    selector = etree.HTML(response.text)
-    token = selector.xpath('//div/input[2]/@value')[0]
-    return token
 
-class login(object):
+
+class Login(object):
     def __init__(self):
         self.headers = {
             'Referer': 'https://github.com',
@@ -20,35 +16,45 @@ class login(object):
         self.post_url = 'https://github.com/session'
         self.logined_url = 'https://github.com/settings/profile'
         self.session = requests.Session()
+    def login(self, email, password):
+        post_data = {
+            'commit': 'Sign in',
+            'utf8':'',
+            'authenticity_token':self.token()[0],
+            'login': email,
+            'password':password
+        }
 
-def login(self, email, password):
-    post_data = {
-        'commit': 'Sign in',
-        'utf8':'',
-        'authenticity_token':self.token(),
-        'login': email,
-        'password':password
-    }
-
-    response = self.session.post(self.post_url, data= post_data, headers = self.headers)
-    if response.status_code == 200:
-        self.dynamics(response.text)
-    
-    response = self.session.get(self.login_url, headers = self.headers)
-    if response.status_code ==200:
-        self.profile = response.text
-
-
-def dynamics(self, html):
-    selector = etree.HTML(html)
-    dynamics = selector.xpath('//div[contains(@class, "news")]//div[contains(@class, "alert")]') 
-    for item in dynamics:
-        dynamic = ' '.join(item.xpath('.//div[@class="title"]//text()')).strip()
-        print(dynamic)
+        response = self.session.post(self.post_url, data= post_data, headers = self.headers)
+        print(response)
+        if response.status_code == 200:
+            self.dynamics(response.text)
+        
+        response = self.session.get(self.login_url, headers = self.headers)
+        if response.status_code ==200:
+            self.profile = response.text
 
 
-def profile(self, html):
-    selector = etree.HTML(html)
-    name = selector.xpath('//input[@id="user_profile_name"]/@value')[0]
-    email = selector.xpath('//select[@id="user_profile_email"]/option[@value!=""]/text()')
-    print(name, email)
+    def dynamics(self, html):
+        selector = etree.HTML(html)
+        dynamics = selector.xpath('//div[contains(@class, "news")]//div[contains(@class, "alert")]') 
+        for item in dynamics:
+            dynamic = ' '.join(item.xpath('.//div[@class="title"]//text()')).strip()
+            print(dynamic)
+
+
+    def profile(self, html):
+        selector = etree.HTML(html)
+        name = selector.xpath('//input[@id="user_profile_name"]/@value')[0]
+        email = selector.xpath('//select[@id="user_profile_email"]/option[@value!=""]/text()')
+        print(name, email)
+    def token(self):
+        response = self.session.get(self.login_url, headers= self.headers)
+        selector = etree.HTML(response.text)
+        token = selector.xpath('//div/input[2]/@value')
+        return token
+if __name__ == "__main__":
+    login = Login()
+    login.login(email="", password='')
+
+
