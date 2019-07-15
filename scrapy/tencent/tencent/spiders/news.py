b093c25c6c6ec9a7f1e29506400d4f405a73b4d3
pengzongge <pengzongge@PengZonggedeMacBook-Pro-2.local>
2019-07-15 17:20:31 +0800

    scrapy

--- scrapy/tencent/tencent/spiders/news.py
+++ scrapy/tencent/tencent/spiders/news.py
@@ -5,17 +5,19 @@ from tencent.items import NewsItem
 
 class NewsSpider(scrapy.Spider):
     name = 'news'
-    # allowed_domains = ['www.qq.com']
-    # start_urls = ['www.qq.com']
-    allowed_domains = ["quotes.toscrape.com"]
-    start_urls = ['http://quotes.toscrape.com/']
+    allowed_domains = ['qq.com']
+    start_urls = ['https://www.qq.com']
+    # allowed_domains = ["quotes.toscrape.com"]
+    # start_urls = ['http://quotes.toscrape.com/']
 
     def parse(self, response):
-        print('====='+response+'================')
-        doc = pq(response)
-        newsList = doc('.yw-list li')
-        for ite in newsList:
-            item = NewsItem()
-            item['title'] = ite.find('a').text()
-            print(item)
-        scrapy.Request(url=start_urls, callback=self.parse)
+        # print('====='+response+'================')
+        news = response.css('.yw-list')
+        print(news)
+        # newsList = doc('.yw-list li')
+        # for ite in newsList:
+        #     item = NewsItem()
+        #     item['title'] = ite.find('a').text()
+        #     print(item)
+        # scrapy.Request(url=start_urls, callback=self.parse)
+        
