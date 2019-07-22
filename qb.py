8f7aa8b2e7709ab3f7a726e5d95ecf8cf9515b60
pengzongge <pengzongge@PengZonggedeMacBook-Pro-2.local>
2019-07-22 17:07:23 +0800

    co

--- qb.py
+++ qb.py
@@ -7,10 +7,10 @@ baseUrl = 'https://www.231ta.com'
 filename = "data.txt"
 
 with open(filename,'a') as f:
-	for i in range(1,300): 
+	for i in range(1,20): 
 		print(i)
-		url = baseUrl+"/Html/60/index-"+str(i)+".html"
-		#url = "https://www.qiushibaike.com/imgrank/page/"+str(i)
+		# url = baseUrl+"/Html/60/index-"+str(i)+".html"
+		url = "https://www.qiushibaike.com/imgrank/page/"+str(i)
 
 		wbdata = requests.get(url)
 		# print(wbdata.apparent_encoding)
@@ -19,15 +19,14 @@ with open(filename,'a') as f:
 		# 对获取到的文本进行解析
 		soup = BeautifulSoup(webData,'lxml')
 		# print(soup)
+
 		# 从解析文件中通过select选择器定位指定的元素，返回一个列表
-		# news_titles = soup.select("div.movie_list ul li a h3")
+		news_titles = soup.select(".content span")
+		for n in news_titles:
+			print(n.get_text())
+			f.write(n.get_text()+"\n")
 
-		# for n in news_titles:
-		# 	print(n.get_text())
-		# 	f.write(n.get_text()+"\n")
-
-		items = soup.select("div.movie_list ul li")
-
-		for n in items:
-			f.write(n.find('h3').get_text()+'\n')
-			f.write(baseUrl+n.a.attrs['href']+'\n')
+		# items = soup.select("div.movie_list ul li")
+		# for n in items:
+		# 	f.write(n.find('h3').get_text()+'\n')
+		# 	f.write(baseUrl+n.a.attrs['href']+'\n')
