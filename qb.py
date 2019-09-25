a49c634e027da255cc3319a11373def2619d417b
pengzongge <pengzongge@PengZonggedeMacBook-Pro-2.local>
2019-09-25 17:51:47 +0800

    test

--- qb.py
+++ qb.py
@@ -2,15 +2,15 @@
 import requests;
 from bs4 import BeautifulSoup
 
-baseUrl = 'https://www.231ta.com'
+baseUrl = 'https://www.298ca.com'
 
 filename = "data.txt"
 
 with open(filename,'a') as f:
 	for i in range(1,20): 
 		print(i)
-		# url = baseUrl+"/Html/60/index-"+str(i)+".html"
-		url = "https://www.qiushibaike.com/imgrank/page/"+str(i)
+		url = baseUrl+"/Html/60/index-"+str(i)+".html"
+		#url = "https://www.qiushibaike.com/imgrank/page/"+str(i)
 
 		wbdata = requests.get(url)
 		# print(wbdata.apparent_encoding)
@@ -21,12 +21,13 @@ with open(filename,'a') as f:
 		# print(soup)
 
 		# 从解析文件中通过select选择器定位指定的元素，返回一个列表
-		news_titles = soup.select(".content span")
-		for n in news_titles:
-			print(n.get_text())
-			f.write(n.get_text()+"\n")
+		# news_titles = soup.select(".content span")
+		# for n in news_titles:
+		# 	print(n.get_text())
+		# 	f.write(n.get_text()+"\n")
 
-		# items = soup.select("div.movie_list ul li")
-		# for n in items:
-		# 	f.write(n.find('h3').get_text()+'\n')
-		# 	f.write(baseUrl+n.a.attrs['href']+'\n')
+		items = soup.select("div.movie_list ul li")
+		for n in items:
+			f.write(n.find('h3').get_text()+'\n')
+			print(n.find('h3').get_text()+'\n')
+			f.write(baseUrl+n.a.attrs['href']+'\n')
