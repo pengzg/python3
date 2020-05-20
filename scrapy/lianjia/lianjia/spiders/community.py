591f84415980bb5b05b6878b5c7696b2375ef5d4
pengzongge <pengzongge@MacBook-Pro-2.lan>
2020-05-20 16:02:28 +0800

    c

--- scrapy/lianjia/lianjia/spiders/community.py
+++ scrapy/lianjia/lianjia/spiders/community.py
@@ -3,23 +3,45 @@ import scrapy
 from pyquery import PyQuery as pq
 from lianjia.items import LianjiaItem
 from bs4 import BeautifulSoup
+import pymysql
 
 class CommunitySpider(scrapy.Spider):
     
     name = 'community'
     allowed_domains = ['lianjia.com']
 
-    start_urls = ['https://jn.lianjia.com/xiaoqu/pg2/']
+    start_urls = ['https://jn.lianjia.com/xiaoqu/pg80/']
+
+    
 
     def parse(self, response):
+
+        host = '182.92.180.219'
+        pwd = 'SCrLzWZUj1Iz7ZUbAzYU'
+        db = pymysql.connect(host=host, user='root', password=pwd, port=3306, db='sqhz_dev')
+        cursor = db.cursor()
         print(type(response))
         print(response)
         print("1111")
         doc = pq(response.text)
        
 
-        newsList = doc('.xiaoquListItem .title a').items()
+        # newsList = doc('.xiaoquListItem .title a').items()
+        # print(type(newsList))
+        # for news in newsList:
+        #     print(news.text()+'\n')
+        #     print(news.attr['href']+'\n')    
+        newsList = doc('.xiaoquListItem').items()
         print(type(newsList))
         for news in newsList:
-            print(news.text()+'\n')
-            print(news.attr['href']+'\n')    
+            print(news(".title a").text()+'\n')
+            print(news(".positionInfo .district").text()+'\n')
+            print(news(".positionInfo .bizcircle").text()+'\n')
+            print(news(".totalPrice span").text()+'\n')
+            sql = 'INSERT INTO jd_bcm(bc_name,bc_dis,bc_price,bc_biz,bc_url) values(%s, %s, %s, %s, %s)'
+            try:
+                cursor.execute(sql, (news(".title a").text(),news(".positionInfo .district").text(),news(".totalPrice span").text(),news(".positionInfo .bizcircle").text(),news(".title a").attr['href']))
+                db.commit()
+            except:
+                db.rollback() 
+            # print(news[0].attr['href']+'\n') 
